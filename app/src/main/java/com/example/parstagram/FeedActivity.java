feb3337927b1bfed8afde5bb89ea75d7a529e1e0
Emmanuel Onung <emyonung@fb.com>
2022-06-14 16:41:33 -0700

    Created layout for post

---
+++ app/src/main/java/com/example/parstagram/FeedActivity.java
@@ -0,0 +1,17 @@
+package com.example.parstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+    private RecyclerView rvPosts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+        rvPosts = findViewById(R.id.rvPosts);
+    }
+}
